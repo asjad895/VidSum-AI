ff3be014a4b37d4893710d7dda2693485af543b8
Md Asjad <mdasjad895@gmail.com>
2023-08-18 23:25:00 +0530

    updated

--- FRONTEND/static/js/script.js
+++ FRONTEND/static/js/script.js
@@ -1,3 +1,16 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('upload-form');
+    const fileInput = document.getElementById('file-upload');
+    const linkInput = document.getElementById('video-link');
+    form.addEventListener('submit', function(event) {
+        if (!fileInput.files.length && !linkInput.value.trim()) {
+            event.preventDefault();
+            alert('Please select a file or enter a video link.');
+        }
+    });
+});
+
+
 
 
